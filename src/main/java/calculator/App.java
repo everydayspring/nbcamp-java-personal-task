cd20d47617bdc1e1b1977f976afa1d9f72ec93b8
everydayspring <lee.bom.952@gmail.com>
2024-07-24 16:34:22 +0900

    Level1-2 Scanner 사용하여 연산기호 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,5 +15,9 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요:");
         Double num2 = Double.parseDouble(sc.nextLine());
 
+        // 연산 기호를 Character 형태로 입력받기 위해 charAt() 사용
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.nextLine().charAt(0);
+
     }
 }
