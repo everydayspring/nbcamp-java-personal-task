583687a37ecd26cba56809fc22082dddeed94b26
everydayspring <lee.bom.952@gmail.com>
2024-07-24 16:29:46 +0900

    Level1-1 Scanner 사용하여 숫자 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in);
+
+        // Scanner에서 nextInt()와 nextLine() 사용시 공란 입력 방지를 위해 parser 사용
+        // 연산을 위해 Double로 선언
+        System.out.print("첫 번째 숫자를 입력하세요:");
+        Double num1 = Double.parseDouble(sc.nextLine());
+        System.out.print("두 번째 숫자를 입력하세요:");
+        Double num2 = Double.parseDouble(sc.nextLine());
+
     }
 }
