7384472f461fac72087c5a6e5c51275667c39c74
everydayspring <lee.bom.952@gmail.com>
2024-07-24 16:39:02 +0900

    Level1-3 제어문을  사용한 연산, 결과 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,14 +10,38 @@ public class App {
 
         // Scanner에서 nextInt()와 nextLine() 사용시 공란 입력 방지를 위해 parser 사용
         // 연산을 위해 Double로 선언
-        System.out.print("첫 번째 숫자를 입력하세요:");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
         Double num1 = Double.parseDouble(sc.nextLine());
-        System.out.print("두 번째 숫자를 입력하세요:");
+        System.out.print("두 번째 숫자를 입력하세요: ");
         Double num2 = Double.parseDouble(sc.nextLine());
 
         // 연산 기호를 Character 형태로 입력받기 위해 charAt() 사용
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.nextLine().charAt(0);
 
+        double result = 0;
+
+        // 스위치문을 사용한 연산
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                // 두번째 숫자 0에 대한 나눗셈 예외처리
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+                    break;
+                }
+                result = num1 / num2;
+                break;
+        }
+
+        System.out.println("결과: " + result);
     }
 }
